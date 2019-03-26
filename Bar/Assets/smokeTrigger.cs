eb386ddb1152ce70c1352f2ee354504067b8fc79
NawamKarki <48933388+NawamKarki@users.noreply.github.com>
2019-03-26 14:53:23 +1300

    Bar Scenario
    VESPHA - Bar Scenario

---
+++ Bar/Assets/smokeTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smokeTrigger : MonoBehaviour
+{
+
+    //instance of the smoke particle system
+    public ParticleSystem smoke;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Debug.Log("haha");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Debug.Log("haha");
+            smoke.Play();
+        }
+    }
+}
