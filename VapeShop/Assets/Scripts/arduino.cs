9f19696b7e8f1fb6379cbbc23b250dfa7727c49c
NawamKarki <48933388+NawamKarki@users.noreply.github.com>
2019-03-29 09:59:05 +1300

    VapeShop

---
+++ VapeShop/Assets/Scripts/arduino.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.IO.Ports;
+using System;
+//using System.Console;
+
+
+
+public class arduino : MonoBehaviour
+{
+    //[Serializable] GameObject smoke;
+    //public GameObject smok;
+
+    [SerializeField] ParticleSystem smo;
+
+
+
+    //functionality to receive data from the serial port
+    SerialPort sp = new SerialPort("COM5", 12600);
+    string distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            sp.Open(); //open the serial port
+            sp.ReadTimeout = 101;
+            //sp.DataReceived += DataReceivedHandler;
+        }
+
+        catch (Exception e)
+        {
+            Debug.Log("Could not open serial port: " + e.Message);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int ard1 = int.Parse(sp.ReadLine());
+        //Debug.Log(ard1/100);
+
+        if (ard1 / 100 > 400)
+        {
+            //smo.startSize = 0.1f; //controls the size of the smoke
+            Debug.Log(ard1 / 100);
+            smo.Play();
+        }
+    }
+
+    //private void DataReceivedHandler(
+    //                              object sender,
+    //                              SerialDataReceivedEventArgs e)
+    //{
+    //    SerialPort sp1 = (SerialPort)sender;
+    //    string distance = sp1.ReadLine();
+    //    Debug.Log(distance);
+    //}
+}
---
+++ VapeShop/Assets/Scripts/moveCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour
+{
+    public float Speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal") * Speed/100;
+        float zAxisValue = Input.GetAxis("Vertical") * Speed/100;
+
+        transform.position = new Vector3(transform.position.x + xAxisValue, transform.position.y, transform.position.z + zAxisValue);
+    }
+}
